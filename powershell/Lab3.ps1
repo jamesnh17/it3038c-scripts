8500c91563bbd5593494f7ef9a0079a04b616cb8
jamesnh17 <huffnj@mail.uc.edu>
2022-09-14 15:53:39 -0400

    powershell files

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,16 @@
+﻿function getIP{
+    (Get-NetIPAddress).IPv4Address | Select-String "192*"
+}
+
+function GDate{
+    Get-Date -Format "dddd MM/dd/yyyy"
+}
+
+
+$IP = getIP
+$HN = hostname
+$HV = $Host.Version.Major
+$GD = GDate
+$Body = "This machine's IP is $IP. User is Administrator. Hostname is $HN. PowerShell Version $HV. Today's Date is $GD."
+
+$Body | Out-File C:\Lab3.txt
