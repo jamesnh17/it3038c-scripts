4f8ae7ac2111d85da3f3ce0bc7d0bf50958cc437
jamesnh17 <huffnj@mail.uc.edu>
2022-10-13 23:30:02 -0400

    finally got server to work

--- node/server.js
+++ node/server.js
@@ -4,34 +4,46 @@ const os = require("os");
 const ip = require('ip');
 
 http.createServer((req, res) => {
-  if (req.url === "/") {
-      fs.readFile("./public/index.html", "UTF-8", (err, body) => {
-      res.writeHead(200, {"Content-Type": "text/html"});
-      res.end(body);
-    });
-  } else if(req.url.match("/sysinfo")) {
-    myHostName=os.hostname();
-    html=`
-    <!DOCTYPE html>
-    <html>
-      <head>
-        <title>Node JS Response</title>
-      </head>
-      <body>
-        <p>Hostname: ${myHostName}</p>
-        <p>IP: ${ip.address()}</p>
-        <p>Server Uptime: </p>
-        <p>Total Memory: </p>
-        <p>Free Memory: </p>
-        <p>Number of CPUs: </p>
-      </body>
-    </html>`
-    res.writeHead(200, {"Content-Type": "text/html"});
-    res.end(html);
-  } else {
-    res.writeHead(404, {"Content-Type": "text/plain"});
-    res.end(`404 File Not Found at ${req.url}`);
-  }
+    if (req.url === "/") {
+        fs.readFile("./Public/index.html", "UTF-8", (err, body) => {
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.end(body);
+        });
+    } else if(req.url.match("/sysinfo")) {
+        myHostName=os.hostname();
+        totalMemory=os.totalmem();
+        memoryMB=(totalMemory / (1024 * 1024));
+        freeMemory=os.freemem();
+        freeMB=(freeMemory / (1024 * 1024));
+
+        // Uptime Variables
+        totalSeconds=os.uptime();
+        upDays=Math.floor(totalSeconds / 86400);
+        upHours=Math.floor((totalSeconds / 3600) - (upDays * 24));
+        upMinutes=Math.floor(totalSeconds % 3600 / 60);
+        upSeconds=Math.floor(totalSeconds % 60);
+
+        html=`
+        <!DOCTYPE html>
+        <html>
+          <head>
+            <title>Node JS Response</title>
+          </head>
+          <body>
+            <p>Hostname: ${myHostName}</p>
+            <p>IP: ${ip.address()}</p>
+            <p>Server Uptime: ${upDays} Days ${upHours} hours ${upMinutes} minutes ${upSeconds} seconds </p>
+            <p>Total Memory: ${memoryMB} MB</p>
+            <p>Free Memory: ${freeMB} MB</p>
+            <p>Number of CPUs: ${os.cpus().length} CPU's</p>
+          </body>
+        </html>`
+        res.writeHead(200, {"Content-Type": "text/html"});
+        res.end(html);
+    } else{
+        res.writeHead(404, {"Content-Type": "text/plain"});
+        res.end(`404 File Not Found at ${req.url}`);
+    }
 }).listen(3000);
 
 console.log("Server listening on port 3000");
