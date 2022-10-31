387e18eef64eec69ec93422bbe0f56ef0e7d1f57
jamesnh17 <huffnj@mail.uc.edu>
2022-10-30 23:56:47 -0400

    Lab8 finished

--- python/Lab8.py
+++ python/Lab8.py
@@ -1,7 +1,17 @@
-import requests
+import requests, re
 from bs4 import BeautifulSoup
 
-URL = ""
+# Lab simply scrapes for my 'Lab8' link from my own github page
+
+URL = "https://github.com/jamesnh17/it3038c-scripts/tree/main/python"
 page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
+
+results = soup.find(title="Lab8.py")
+
+#print(results.prettify())
+
+tags = soup.find_all("a", {"href":re.compile('(Lab8.py)')})
+for a in tags:
+    print(a.get('href'))
